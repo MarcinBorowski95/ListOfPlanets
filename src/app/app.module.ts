a74341b76e5df54bf0ecfb815550b8ed8fe98c32
MarcinBorowski95 <bomarc2@gmail.com>
2019-02-16 12:37:56 +0100

    setup Ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { reducers, metaReducers } from './store/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppEffects } from './store/effects/app.effects';
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -12,9 +18,13 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    StoreModule.forRoot(reducers, {metaReducers}),
+    EffectsModule.forRoot([AppEffects]),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
