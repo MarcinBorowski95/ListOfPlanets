e0ec9bdacc436e549aa678120f978a86289a57ef
MarcinBorowski95 <bomarc2@gmail.com>
2019-02-18 21:54:03 +0100

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,7 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
 const routes: Routes = [
   {path: 'planet-list', component: PlanetListComponent, data: {title: 'Planet list'}},
   {path: 'planet/:id', component: PlanetComponent},
-  {path: '', redirectTo: 'planet-list', pathMatch: 'full'},
-  {path: '**', component: PageNotFoundComponent}
+  {path: '**', redirectTo: 'planet-list', pathMatch: 'full'},
 ];
 
 @NgModule({
